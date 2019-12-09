91d6f5bddf481301c41219a71d536354ffb49f82
Louis Li <louis@shoplineapp.com>
2019-12-09 19:23:46 +0800

    build index

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navigation from './layouts/Navigation';
+import Content from './layouts/Content';
+import Footer from './layouts/Footer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navigation />
+        <Content />
+        <Footer />
       </div>
     );
   }
---
+++ src/layouts/Content.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import CardColumns from 'react-bootstrap/CardColumns';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
+import ItemImg from './img.svg';
+import { MdLocationOn } from "react-icons/md";
+import { MdAlarm } from "react-icons/md";
+
+class Content extends Component {
+  render() {
+    return (
+      <Container>
+        <CardColumns>
+          <Card>
+            <Card.Img variant="top" src={ItemImg} />
+            <Card.Body>
+              <Card.Title>FREE MOVIE + $5 HAPPY HOUR SPECIALS + FREE POPCORN @The Soda Factory</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>
+               <MdAlarm /> Mon, 09 Dec · 5:00 pm
+              </ListGroupItem>
+              <ListGroupItem>
+                <MdLocationOn /> Sydney, Australia
+              </ListGroupItem>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Detail</Card.Link>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src={ItemImg} />
+            <Card.Body>
+              <Card.Title>FREE MOVIE + $5 HAPPY HOUR SPECIALS + FREE POPCORN @The Soda Factory</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>
+               <MdAlarm /> Mon, 09 Dec · 5:00 pm
+              </ListGroupItem>
+              <ListGroupItem>
+                <MdLocationOn /> Sydney, Australia
+              </ListGroupItem>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Detail</Card.Link>
+            </Card.Body>
+          </Card>
+        </CardColumns>
+      </Container>
+    );
+  }
+}
+
+export default Content;
